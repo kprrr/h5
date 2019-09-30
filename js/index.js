fe7d63ccfd1ba44622dc0ed774c091363fc28dab
kprrr <495782781@qq.com>
2019-09-30 16:47:15 +0800

    touch滑动事件完成

--- js/index.js
+++ js/index.js
@@ -75,23 +75,112 @@ function bannerEffect() {
     // window.onresize = function () {
 
     // }
-    var index = 1 ;
-    setInterval(() => {
-        //5.2添加过度动画效果
-        imgBox.style.transition = "left 0.5s ease-in-out";
-        imgBox.style.left = -((index++)*bannerWidth) + "px";
+    var index = 1;
+    var timer = function () {
+        return setInterval(() => {
+            index++
+            //5.2添加过度动画效果
+            imgBox.style.transition = "left 0.5s ease-in-out";
+            imgBox.style.left = -(index * bannerWidth) + "px";
+            // console.log("当前index:" + index);
 
-        if(index == countImg -1 ){ 
-            setTimeout(() => {
-                index = 1;
-                imgBox.style.transition = "none";
-                // imgBox.classList.add("noTransition");
-                // imgBox.setAttribute("class","noTransition");
-                imgBox.style.left = -bannerWidth + "px";
-            }, 500);
+            //当到倒数第二张时，回到第一张
+            if (index == countImg - 1) {
+                setTimeout(() => {
+                    index = 1;
+                    imgBox.style.transition = "none";
+                    // imgBox.classList.add("noTransition");
+                    // imgBox.setAttribute("class","noTransition");
+                    imgBox.style.left = -bannerWidth + "px";
+                }, 500);
+            }
+
+
+        }, 1000);
+        // return timeId;
+    }
+
+    var timeId = timer();
+
+    //滑动操作
+
+    //2.给imgBox清楚浮动，用于增加宽度的，否则无法触发touch事件
+    //3.监听touch事件
+    var startX, moveX, distanceX, currentLeftX;
+    //3.1 开始滑动
+    imgBox.addEventListener("touchstart", function (e) {
+        //1.清除定时器
+        clearInterval(timeId);
+        startX = e.targetTouches[0].clientX;
+        currentLeftX = imgBox.offsetLeft;
+        console.log("当前index:"+index);
+
+        // console.log("当前left值："+imgBox.offsetLeft+";"+"index计算后的left值"+-index*bannerWidth);
+
+    });
+    //3.2 滑动中
+    imgBox.addEventListener("touchmove", function (e) {
+        moveX = e.targetTouches[0].clientX;
+        distanceX = moveX - startX;
+        imgBox.style.transition = "none";
+        // imgBox.style.left = (imgBox.offsetLeft + distanceX)+"px";
+        imgBox.style.left = (currentLeftX + distanceX) + "px";
+
+    });
+    //3.3 滑动结束
+    imgBox.addEventListener("touchend", function (e) {
+        //3.3.1 如果滑动但没达到翻页距离，则回弹
+        // console.log("滑动距离："+distanceX+";判断标准"+bannerWidth / 3);
+        var standard = bannerWidth / 3;
+        var absDistance = Math.abs(distanceX);
+        if (absDistance > 0 && absDistance < standard) {
+            imgBox.style.transition = "none";
+            imgBox.style.left = -index * bannerWidth + "px";
         }
+        //3.3.2 如果滑动并且达到翻页距离，则翻页
+        if (absDistance >= standard) {
+            imgBox.style.transition = "none";
+            if (distanceX > 0) { //3.3.2.1 手往右滑，翻到前面一页
+                //注意：这里不能这样计算，需要用index来计算，否则后续使用index时就会失败
+                // imgBox.style.left = -(index - 1) * bannerWidth + "px";
+
+                //如果到了第一页,则跳转到最后一页
+                if (index == 1) {
+                    index = countImg - 1;
+                    imgBox.style.left = -index * bannerWidth + "px";
+                } else {
+                    index--;
+                    //滑动效果
+                    imgBox.style.transition = "left 0.5s ease-in-out";
+                    imgBox.style.left = -index * bannerWidth + "px";
+                }
+
+            } else { //3.3.2.2 手往坐滑，翻到后面一页
+                //注意：这里不能这样计算，需要用index来计算，否则后续使用index时就会失败
+                // imgBox.style.left = -(index + 1) * bannerWidth + "px";
+                //如果到了最后一页，则跳转到第一页
+                if (index == countImg - 2) {
+                    index = 1;
+                    imgBox.style.left = -index * bannerWidth + "px";
+                } else {
+                    index++;
+                    imgBox.style.transition = "left 0.5s ease-in-out";
+                    imgBox.style.left = -index * bannerWidth + "px";
+                }
+
+            }
+
+        }
+        //3.3.3 如果没有滑动 ，则不做任何处理
+        if (absDistance == 0) {
+
+        }
+        timeId = timer();
+    });
+
+    //使用 webkitTransitionEnd ，可以监听当前元素的过渡效果执行完毕，当一个元素的过渡效果执行完毕后，会触发这个事件
+    // imgBox.addEventListener("webkitTransitionEnd",function name(params) {
         
-       
-    }, 1000);
+    // })
 
 }
